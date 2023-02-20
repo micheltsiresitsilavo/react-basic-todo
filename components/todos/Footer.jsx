8af37c8926ee0f175203c50028cd11ace623710e
Michel Tsilavo <micheltsilavodev@protonmail.com>
2023-02-20 07:47:57 -0300

    Komita voalohany

---
+++ components/todos/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Create with React.js. Basic functionality, logic in single file</p>
+      <a href="">@Michel Tsilavo</a>
+    </footer>
+  );
+};
+
+export default Footer;
